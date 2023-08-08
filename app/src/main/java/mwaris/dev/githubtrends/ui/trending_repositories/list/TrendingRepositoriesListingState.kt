5246effb95301896ef03830d41db6fcf070da377
mwaris <warisdgk@gmail.com>
2023-08-09 00:59:46 +0500

    Green test to validate backend error in trending listing

--- app/src/main/java/mwaris/dev/githubtrends/ui/trending_repositories/list/TrendingRepositoriesListingState.kt
+++ app/src/main/java/mwaris/dev/githubtrends/ui/trending_repositories/list/TrendingRepositoriesListingState.kt
@@ -3,5 +3,7 @@ package mwaris.dev.githubtrends.ui.trending_repositories.list
 import mwaris.dev.githubtrends.data.entities.Repository
 
 sealed class TrendingRepositoriesListingState {
+    object BackendError : TrendingRepositoriesListingState()
+
     data class GithubRepositories(val repos: List<Repository>) : TrendingRepositoriesListingState()
 }
---
+++ app/src/test/java/mwaris/dev/githubtrends/trending_repositories/list/FailingTrendingRepositoriesListTest.kt
@@ -0,0 +1,35 @@
+package mwaris.dev.githubtrends.trending_repositories.list
+
+import mwaris.dev.githubtrends.InstantExecutorExtension
+import mwaris.dev.githubtrends.data.repositories.ITrendingListRepository
+import mwaris.dev.githubtrends.data.repositories.TrendingListRepository
+import mwaris.dev.githubtrends.ui.trending_repositories.list.TrendingRepositoriesListingState
+import mwaris.dev.githubtrends.ui.trending_repositories.list.TrendingRepositoriesListingViewModel
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+@ExtendWith(InstantExecutorExtension::class)
+class FailingTrendingRepositoriesListTest {
+
+    @Test
+    fun failingBackendError() {
+        val trendingListRepository = UnavailableTrendingRepositoryListRepository()
+        val viewModel = TrendingRepositoriesListingViewModel(trendingListRepository)
+
+        viewModel.getTrendingGithubRepositoriesList()
+
+        Assertions.assertEquals(
+            TrendingRepositoriesListingState.BackendError,
+            viewModel.trendingReposListingState.value
+        )
+    }
+
+    class UnavailableTrendingRepositoryListRepository : ITrendingListRepository {
+        override fun getTrendingGithubRepositoriesList(): TrendingRepositoriesListingState {
+            return TrendingRepositoriesListingState.BackendError
+        }
+
+    }
+
+}
