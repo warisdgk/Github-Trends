c79697fa757f82913eb709b1f09270d0de3bdf61
mwaris <warisdgk@gmail.com>
2023-08-11 03:12:24 +0500

    Green UI test to validate empty trending repos list

--- app/src/androidTest/java/mwaris/dev/githubtrends/trending_repositories/TrendingRepositoriesListUITest.kt
+++ app/src/androidTest/java/mwaris/dev/githubtrends/trending_repositories/TrendingRepositoriesListUITest.kt
@@ -1,7 +1,13 @@
 package mwaris.dev.githubtrends.trending_repositories
 
 import androidx.activity.ComponentActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
@@ -40,7 +46,19 @@ class TrendingRepositoriesListUITest {
     fun showsEmptyTrendingRepositoriesList(){
         composeTestRule.setContent {
             GithubTrendsTheme {
-                APIUnreachable("Something went wrong")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    LazyColumn(
+                        modifier = Modifier.testTag("trending-repositories-list")
+                    ) {
+                        items(count = 15) {
+                            Text(text = "Trending Repo")
+                        }
+                    }
+                    APIUnreachable("Something went wrong")
+                }
             }
         }
 
--- app/src/main/java/mwaris/dev/githubtrends/MainActivity.kt
+++ app/src/main/java/mwaris/dev/githubtrends/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
@@ -35,6 +36,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    LazyColumn(
+                        modifier = Modifier.testTag("trending-repositories-list")
+                    ) {
+                        items(count = 15) {
+                            Text(text = "Trending Repo")
+                        }
+                    }
                     APIUnreachable("Something went wrong")
                 }
             }
