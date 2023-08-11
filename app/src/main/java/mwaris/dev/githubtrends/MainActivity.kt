d9ca5c78e9bf60fe38a99f50467d38a2d0af4b4b
mwaris <warisdgk@gmail.com>
2023-08-12 01:20:36 +0500

    Improved Trending repos list UI further

--- app/src/main/java/mwaris/dev/githubtrends/MainActivity.kt
+++ app/src/main/java/mwaris/dev/githubtrends/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
@@ -18,13 +21,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import mwaris.dev.githubtrends.ui.theme.GithubTrendsTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,12 +47,44 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier.testTag("trending-repositories-list")
                     ) {
                         items(count = 15) {
-                            Text(text = "Trending Repo")
+                            TrendingRepositoryItem()
                         }
                     }
                     APIUnreachable("Something went wrong")
                 }
             }
+        }
+    }
+}
+
+@Composable
+private fun TrendingRepositoryItem() {
+    Row(
+        Modifier
+            .padding(all = 15.dp)
+            .fillMaxWidth()
+    ) {
+        AsyncImage(
+            model = "https://avatars.githubusercontent.com/u/4314092?v=4",
+            contentDescription = "avatar",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(64.dp)
+                .clip(CircleShape)
+                .border(2.dp, Color.Gray, CircleShape)
+        )
+        Column(
+            Modifier
+                .padding(start = 15.dp)
+                .fillMaxWidth()
+        ) {
+            Text("go")
+            Spacer(modifier = Modifier.size(5.dp))
+            Text(
+                "golang/go"
+            )
+            Spacer(modifier = Modifier.size(10.dp))
+            Text("The Go programming language")
         }
     }
 }
@@ -92,8 +131,22 @@ fun APIUnreachable(name: String, modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun APIUnreachablePreview() {
     GithubTrendsTheme {
         APIUnreachable("Something went wrong")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TrendingRepositoriesListPreview() {
+    GithubTrendsTheme {
+        LazyColumn(
+            modifier = Modifier.testTag("trending-repositories-list")
+        ) {
+            items(count = 15) {
+                TrendingRepositoryItem()
+            }
+        }
     }
 }
