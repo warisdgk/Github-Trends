d076da14e53a8321c42993bbaa5bc0b5c03149dc
mwaris <warisdgk@gmail.com>
2023-08-14 00:34:39 +0500

    Introduced base classes for viewmodels to offload screen state management

---
+++ app/src/main/java/mwaris/dev/githubtrends/base/BaseStateViewModel.kt
@@ -0,0 +1,14 @@
+package mwaris.dev.githubtrends.base
+
+import android.os.Parcelable
+import androidx.lifecycle.SavedStateHandle
+
+abstract class BaseStateViewModel<DataState : Parcelable>(
+    savedStateHandle: SavedStateHandle,
+    initDataState: DataState
+) : BaseViewModel<DataState>() {
+
+    override val mutableDataState by lazy {
+        savedStateHandle.getLiveData("mainMutableDataState", initDataState)
+    }
+}
---
+++ app/src/main/java/mwaris/dev/githubtrends/base/BaseViewModel.kt
@@ -0,0 +1,15 @@
+package mwaris.dev.githubtrends.base
+
+import android.os.Parcelable
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+abstract class BaseViewModel<DataState : Parcelable>(
+) : ViewModel() {
+
+    protected abstract val mutableDataState: MutableLiveData<DataState>
+    open val dataState: LiveData<DataState> by lazy { mutableDataState }
+
+    protected fun currentState(): DataState = dataState.value!!
+}
