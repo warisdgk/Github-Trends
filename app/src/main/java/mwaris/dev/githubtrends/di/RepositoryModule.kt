b87cf38b627fda851d0ce9d9e0f9acfd4fdb7ab9
mwaris <warisdgk@gmail.com>
2023-08-15 01:59:19 +0500

    Added network monitoring relevant classes to di

--- app/src/main/java/mwaris/dev/githubtrends/di/RepositoryModule.kt
+++ app/src/main/java/mwaris/dev/githubtrends/di/RepositoryModule.kt
@@ -1,9 +1,13 @@
 package mwaris.dev.githubtrends.di
 
+import android.content.Context
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import mwaris.dev.githubtrends.base.ConnectivityManagerNetworkMonitor
+import mwaris.dev.githubtrends.base.NetworkMonitor
 import mwaris.dev.githubtrends.data.repositories.ITrendingListRepository
 import mwaris.dev.githubtrends.data.repositories.TrendingListRepository
 import javax.inject.Singleton
@@ -16,5 +20,11 @@ object RepositoryModule {
     @Singleton
     fun providesITrendingListRepository(): ITrendingListRepository {
         return TrendingListRepository()
+    }
+
+    @Provides
+    @Singleton
+    fun providesNetworkMonitor(@ApplicationContext context: Context): NetworkMonitor {
+        return ConnectivityManagerNetworkMonitor(context)
     }
 }
